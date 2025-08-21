46f8913d60a56871e8423f5f4cd868f9adc9ebad
niraj2024407 <neerajkumar04051998@gmail.com>
2025-08-22 05:04:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //your JS code here. If required.
 function toUppercase() {
-	document.getElementById("fname").addEventListener("blur", function() {
-      this.value = this.value.toUpperCase();
+	 document.getElementById('fname').addEventListener('blur', function() {
+            this.value = this.value.toUpperCase();
+        });
 }
