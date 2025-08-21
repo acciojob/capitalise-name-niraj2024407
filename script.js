ab7aab3aa6fbd943cf8421eadb4c078635ac4661
niraj2024407 <neerajkumar04051998@gmail.com>
2025-08-22 04:51:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
 function toUppercase() {
-      let inputField = document.getElementById("fname");
-      inputField.value = inputField.value.toUpperCase();
+	document.getElementById("fname").addEventListener("blur", function() {
+      this.value = this.value.toUpperCase();
 }
